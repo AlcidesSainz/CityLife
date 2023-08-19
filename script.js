e325109a1d8c15f43ec6a34c5817e2dc4dbdb4af
Alcides Sainz <alcidessainz99@gmail.com>
2023-08-19 17:49:00 -0500

    avion terminado

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-function airplaneFlying() {
+function objectsAnimation() {
   const airplane = document.getElementById("plane");
-
   anime({
     targets: airplane,
-    translateX: 100,
-    translateY: -200,
-    easing: "easeInOutExpo",
+    translateX: "+=1700", // Mover hacia la derecha
+    translateY: "-=390", // Mantener la misma coordenada Y
+    easing: "linear",
     duration: 5000,
     loop: true,
-
-
+    duration: 30000,
+    begin: function (anim) {
+      airplane.style.transform = "translate(-10, -250px)"; // Reiniciar la posición Y
+    },
   });
 }
