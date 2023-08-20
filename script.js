1fef1b21fd4fdbb04bd9d3f0039d9eb44af8044e
Alcides Sainz <alcidessainz99@gmail.com>
2023-08-20 11:26:55 -0500

    isDay function added

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-function objectsAnimation() {
+function isDay() {
+  const backgroundImg = document.getElementById("backgroundImg");
+  backgroundImg.src = "img/sunny2.jpg.png";
+
   const airplane = document.getElementById("plane");
+  airplane.src = "img/plane-silhouette-th.png";
   anime({
     targets: airplane,
     translateX: "+=1700", // Mover hacia la derecha
-    translateY: "-=390", // Mantener la misma coordenada Y
+    translateY: "-=600", // Mantener la misma coordenada Y
     easing: "linear",
     duration: 5000,
     loop: true,
@@ -15,9 +19,13 @@ function objectsAnimation() {
 
   const clouds = document.getElementById("cloud");
   const clouds1 = document.getElementById("cloud1");
-  const clouds2 = document.getElementById("cloud2");
   const clouds3 = document.getElementById("cloud3");
   const clouds4 = document.getElementById("cloud4");
+
+  clouds.src = "img/cloud.png";
+  clouds1.src = "img/cloud 2.png";
+  clouds3.src = "img/cloud.png";
+  clouds4.src = "img/cloud 2.png";
 
   anime({
     targets: clouds,
@@ -29,13 +37,6 @@ function objectsAnimation() {
   anime({
     targets: clouds1,
     translateY: 5,
-    direction: "alternate",
-    loop: true,
-    easing: "easeInOutSine",
-  });
-  anime({
-    targets: clouds2,
-    translateY: -5,
     direction: "alternate",
     loop: true,
     easing: "easeInOutSine",
@@ -56,6 +57,7 @@ function objectsAnimation() {
   });
 
   const spiderman = document.getElementById("spiderman");
+  spiderman.src = "img/spiderman2.png";
 
   anime({
     targets: spiderman,
@@ -66,6 +68,16 @@ function objectsAnimation() {
   });
 
   const crows = document.getElementById("container-crow");
+  const crow = document.getElementById("crow");
+  const crow1 = document.getElementById("crow1");
+  const crow2 = document.getElementById("crow2");
+  const crow3 = document.getElementById("crow3");
+
+  crow.src = "img/crow1.gif";
+  crow1.src = "img/crow1.gif";
+  crow2.src = "img/crow1.gif";
+  crow3.src = "img/crow1.gif";
+
   anime({
     targets: crows,
     translateX: 900,
@@ -75,11 +87,15 @@ function objectsAnimation() {
   });
 
   const crows2 = document.getElementById("container-crow2");
+
+  crows2.innerHTML =
+    '<div><img id="crow" src="img/crow1.gif" alt="" /><img id="crow1" src="img/crow1.gif" alt="" /></div><div><img id="crow2" src="img/crow1.gif" alt="" /><img id="crow3" src="img/crow1.gif" alt="" /></div>';
+
   anime({
     targets: crows2,
     translateY: -900,
     translateX: 900,
-    delay:2000,
+    delay: 2000,
     easing: "linear",
     duration: 20000,
     loop: true,
